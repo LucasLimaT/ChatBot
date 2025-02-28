cb31fc9c8fa33c6726ba0b083fac3c047cebdb55
Lucas dos Santos Pimentel <116539949+LucasPimentel77@users.noreply.github.com>
2025-02-27 20:43:04 -0400

    adicionando o comando "/atender"

--- teleGROQ.py
+++ teleGROQ.py
@@ -4,6 +4,7 @@ from fila import Fila
 from groq import Groq
 import json
 import telebot
+import requests
 
 load_dotenv()
 # ------------------------------------------------------------------------------
@@ -13,6 +14,7 @@ cliente_groq = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 # bot = telebot.TeleBot("Seu token do telegram")
 # cliente_groq = Groq(api_key='CHAVE DA API DO GROK AQUI!')
+
 
 with open("script.txt", "r", encoding="utf-8") as arquivo:
     script = arquivo.read()
@@ -52,7 +54,6 @@ def processar_mensagem(mensagem):
         "resposta": <texto>
     }
     """
-
     return resposta_dados
 
 def iniciar_fila():
@@ -68,30 +69,32 @@ def iniciar_fila():
     return fila
 
 
-
 fila = iniciar_fila()
 
-
-def salvar_fila(dados):
+def salvar_fila(dados=None):
     global fila
-    preferencias = {"vermelho": 1, "laranja": 2, "amarelo": 3, "verde": 4, "azul": 5}
+    try:
+        preferencias = {"vermelho": 1, "laranja": 2, "amarelo": 3, "verde": 4, "azul": 5}
 
-    fila[dados['cpf']] = dados
-    fila = dict(sorted(fila.items(), key=lambda item: preferencias[item[1]["urgencia"]]))
-    
+        fila[dados['cpf']] = dados
+        fila = dict(sorted(fila.items(), key=lambda item: preferencias[item[1]["urgencia"]]))
+    except:
+        pass
+
     with open("fila.json", "w", encoding="utf-8") as file:
-        json.dump(dados, file, indent=4)
+        json.dump(fila, file, indent=4)
 
 
 
 
 
 
-# ---------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+
 
 @bot.message_handler(commands=["sair"])
 def sair(message):
-    salvar_fila(fila)
+    salvar_fila()
     bot.reply_to(message, "Ok, :(")
     bot.stop_polling()
 
@@ -103,7 +106,22 @@ def mostrar_pulseiras(message):
 
     bot.reply_to(message, texto, parse_mode="HTML")
 
+@bot.message_handler(commands=['atender'])
+def atendimento(message):
+    if len(fila) == 0:
+        aviso = 'fila vazia ninguém para ser atendido no momento'
+        print(aviso)
+    
+    else:
+        for cpf, paciente in fila.items():
+            fila.pop(cpf)
+            break
 
+        aviso = f'proximo paciente: {paciente['nome']}\ncpf: {cpf}\nurgencia: {paciente['urgencia']}'
+        print(f'{paciente['nome']} foi atendido')
+
+    salvar_fila()
+    bot.reply_to(message, aviso)
 
 
 @bot.message_handler(func=lambda message: True)
@@ -112,14 +130,12 @@ def assistente(message):
     resposta = dados.pop("resposta")
     status = dados.pop("pronto")
 
-
     try:
         if status:
             salvar_fila(dados)
             print(f"{dados['nome']} enfileirado!")
     except Exception as error:
         print(f'error: {error}')
-
     bot.reply_to(message, resposta)
     print(f"Recebido do usuario: {message.text}")
 
